e69f85264bc7cb2579a41dbb8f5b10ec95800490
TechSneeker <fellipecarreiro2@gmail.com>
2023-06-28 22:24:20 -0300

    Improvement - adding yandex-token on env variables + closed InputStream

--- src/main/java/br/com/techsneeker/envs/Environment.java
+++ src/main/java/br/com/techsneeker/envs/Environment.java
@@ -2,8 +2,10 @@ package br.com.techsneeker.envs;
 
 import org.yaml.snakeyaml.Yaml;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Collections;
 import java.util.Map;
 
@@ -15,15 +17,35 @@ public class Environment {
     private Map<String, Object> ymlValue;
 
     public Environment() {
+        FileInputStream inputStream = null;
+
         try {
-            this.ymlValue = yml.load(new FileInputStream(PATH));
+            inputStream = new FileInputStream(PATH);
+            this.ymlValue = yml.load(inputStream);
         } catch (FileNotFoundException e) {
+
             throw new RuntimeException("I didn't find your env file :(");
+
+        } finally {
+
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
         }
     }
 
     public String getDiscordToken() {
         var token = (Map<String, Object>) this.ymlValue.getOrDefault("tokens", Collections.emptyMap());
-        return (String) token.getOrDefault("discord-token", "");
+        return (String) token.getOrDefault("discord-token", null);
+    }
+
+    public String getYandexToken() {
+        var token = (Map<String, Object>) this.ymlValue.getOrDefault("tokens", Collections.emptyMap());
+        return (String) token.getOrDefault("yandex-token", null);
     }
 }
